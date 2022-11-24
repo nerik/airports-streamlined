3d18601b2f1a9c7fec689c40b97b2cf272b55351
Erik Escoffier <me@erik.nz>
2022-11-24 12:33:33 +0100

    Fixed issue sorting

--- generate.sh
+++ generate.sh
@@ -3,8 +3,8 @@
 csvcut -c municipality,type,iata_code,latitude_deg,longitude_deg,iso_country source/airports.csv > airports.csv
 ogr2ogr -f "GeoJSON" airports.geojson airports.csv -oo X_POSSIBLE_NAMES=longitude_deg -oo Y_POSSIBLE_NAMES=latitude_deg
 
-csvgrep -c type -r "large|medium" -c scheduled_service -r "yes" source/airports.csv | csvcut -c municipality,type,iata_code,latitude_deg,longitude_deg,iso_country > airports_large_medium.csv
+csvgrep -c scheduled_service -r "yes" source/airports.csv | csvgrep -c type -r "large|medium" | csvcut -c municipality,iata_code,latitude_deg,longitude_deg,iso_country > airports_large_medium.csv
 ogr2ogr -f "GeoJSON" airports_large_medium.geojson airports_large_medium.csv -oo X_POSSIBLE_NAMES=longitude_deg -oo Y_POSSIBLE_NAMES=latitude_deg
 
-csvgrep -c type -r "large" -c scheduled_service -r "yes" source/airports.csv | csvcut -c municipality,iata_code,latitude_deg,longitude_deg,iso_country > airports_large.csv
+csvgrep -c scheduled_service -r "yes" source/airports.csv | csvgrep -c type -r "large" | csvcut -c municipality,iata_code,latitude_deg,longitude_deg,iso_country > airports_large.csv
 ogr2ogr -f "GeoJSON" airports_large.geojson airports_large.csv -oo X_POSSIBLE_NAMES=longitude_deg -oo Y_POSSIBLE_NAMES=latitude_deg
