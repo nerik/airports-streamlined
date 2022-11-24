e18dc97a12823564d3a89b3138dfff30046cbd3b
Erik Escoffier <me@erik.nz>
2022-11-24 12:19:07 +0100

    Produce different granularities

--- generate.sh
+++ generate.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-csvgrep -c type -r "large|medium" source/airports.csv | csvcut -c iata_code,latitude_deg,longitude_deg,iso_country,municipality > airports.csv
-
+csvcut -c municipality,type,iata_code,latitude_deg,longitude_deg,iso_country source/airports.csv > airports.csv
 ogr2ogr -f "GeoJSON" airports.geojson airports.csv -oo X_POSSIBLE_NAMES=longitude_deg -oo Y_POSSIBLE_NAMES=latitude_deg
+
+csvgrep -c type -r "large|medium" source/airports.csv | csvcut -c municipality,type,iata_code,latitude_deg,longitude_deg,iso_country > airports_large_medium.csv
+ogr2ogr -f "GeoJSON" airports_large_medium.geojson airports_large_medium.csv -oo X_POSSIBLE_NAMES=longitude_deg -oo Y_POSSIBLE_NAMES=latitude_deg
+
+csvgrep -c type -r "large" source/airports.csv | csvcut -c municipality,iata_code,latitude_deg,longitude_deg,iso_country > airports_large.csv
+ogr2ogr -f "GeoJSON" airports_large.geojson airports_large.csv -oo X_POSSIBLE_NAMES=longitude_deg -oo Y_POSSIBLE_NAMES=latitude_deg
